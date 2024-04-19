c274005b1581e2c15f42a2b4cba46d42f99cd322
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-19 19:49:17 +0800

    added gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import Image from "./components/Image/Image";
 
 const App = () => {
-  const [value, setValue] = useState("");
-  const [submittedValue, setSubmittedValue] = useState([]);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    if (value.trim() === "") {
-      e.preventDefault();
-      alert("enter text");
-      return;
-    }
-    e.preventDefault();
-    // Update the submitted value when the form is submitted
-    setSubmittedValue([...submittedValue, value]);
-    // Clear the input field after submission
-    setValue("");
-  };
-
-  const handleDelete = (index) => {
-    const newState = submittedValue.filter((_, idx) => idx !== index);
-    setSubmittedValue(newState);
-  };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="bg-red-200"
-          placeholder="enter text"
-          name="enterText"
-          value={value}
-          onChange={handleChange}
-        />
-        <button type="submit">submit</button>
-      </form>
-
-      {/* Render the submitted values only if it's not empty */}
-      {submittedValue.length > 0 && (
-        <div>
-          {submittedValue.map((item, idx) => (
-            <div key={idx}>
-              {item}
-              <button onClick={() => handleDelete(idx)}>delete</button>
-            </div>
-          ))}
-        </div>
-      )}
+      <Image />
     </div>
   );
 };
