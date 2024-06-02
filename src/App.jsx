73f5c91e996ca5b80b136a7612b277a7b449b70f
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-02 20:12:34 +0800

    added edit by index exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,70 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
+const data = [
+  {
+    title: "hello",
+  },
+  {
+    title: "world",
+  },
+];
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [inputVal, setInputVal] = useState("");
-  const [modalEdit, setModalEdit] = useState(false);
-  const [currentTodo, setCurrentTodo] = useState({ index: null, text: "" });
-
-  const handleAddTodo = () => {
-    if (inputVal.trim() === "") {
-      alert("please add todo");
-      return;
-    }
-    setTodos([...todos, inputVal]);
-    setInputVal("");
-  };
-
-  const handleDelete = (index) => {
-    const deleteTodo = todos.filter((_, idx) => idx !== index);
-    setTodos(deleteTodo);
-  };
+  const [input, setInput] = useState("");
+  const [currentVal, setCurrentVal] = useState(data);
+  const [edit, setEdit] = useState(false);
+  const [current, setCurrent] = useState({ index: null, text: "" });
 
   const handleEdit = (index) => {
-    setCurrentTodo({ index, text: todos[index] });
-    setModalEdit(true);
+    setCurrent({ index: index, title: currentVal[index].title });
+    setInput(currentVal[index].title);
+    setEdit(true);
   };
 
   const handleUpdate = () => {
-    if (currentTodo.text.trim() === "") {
-      alert("please enter a valid todo");
+    if (input.trim() === "") {
+      alert("please add text");
       return;
     }
-    const updatedTodos = todos.map((todo, idx) =>
-      idx === currentTodo.index ? currentTodo.text : todo
+
+    setCurrentVal((currentVal) =>
+      currentVal.map((item, index) =>
+        index === current.index ? { ...item, title: input } : item
+      )
     );
-    setTodos(updatedTodos);
-    setModalEdit(false);
-    setCurrentTodo({ index: null, text: "" });
+    setInput(""); //clear input fields when submit
+    setEdit(false);
   };
-
   return (
-    <div className="max-w-[900px] m-auto relative">
-      <div className="flex items-center justify-center m-10">
-        <input
-          type="text"
-          className="border border-slate rounded bg-slate-50 p-2"
-          placeholder="add todo"
-          onChange={(e) => setInputVal(e.target.value)}
-          value={inputVal}
-        />
-        <button
-          className="bg-blue-600 text-white p-2 rounded hover:bg-blue-500"
-          onClick={handleAddTodo}
-        >
-          Add Todo
-        </button>
-      </div>
-      {todos.length <= 0 ? (
-        "No todos left"
-      ) : (
-        <ul>
-          {todos.map((item, index) => (
-            <li
-              key={index}
-              className="border border-slate-50 rounded p-2 flex items-center justify-between bg-white shadow-md "
+    <div className="w-full">
+      <ul className="max-w-[700px] m-auto">
+        {currentVal.map((item, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-between my-2 rounded bg-black text-white p-2"
+          >
+            {item.title}
+            <button
+              className="bg-blue-600 text-white rounded p-2 hover:bg-blue-700"
+              onClick={() => handleEdit(index)}
             >
-              {item}
-              <div className="flex items-center gap-3">
-                <button
-                  className="bg-blue-600 text-white rounded p-2"
-                  onClick={() => handleEdit(index)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="bg-red-600 text-white rounded p-2"
-                  onClick={() => handleDelete(index)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {modalEdit && (
-        <div className="fixed left-0 top-0 right-0 bottom-0 flex items-center justify-center w-full bg-slate-200 h-full">
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
+      {edit && (
+        <div className="max-w-[700px] m-auto flex items-center justify-between gap-3">
           <input
+            className="border border-slate-400 p-2 rounded w-full"
             type="text"
-            className="bg-gray-500 rounded p-2 text-white w-full"
-            placeholder="Edit Todo"
-            onChange={(e) =>
-              setCurrentTodo({ ...currentTodo, text: e.target.value })
-            }
-            value={currentTodo.text}
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            placeholder="edit"
           />
           <button
-            className="bg-blue-600 rounded p-2 text-white"
+            className="rounded p-2 text-white bg-green-600"
             onClick={handleUpdate}
           >
-            Update
+            update
           </button>
         </div>
       )}
