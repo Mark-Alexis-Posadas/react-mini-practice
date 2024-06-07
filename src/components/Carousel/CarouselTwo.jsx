ed704ef9aefee6f9cd3289e2f7871b79c375d55d
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-07 08:09:30 +0800

    added carousel dot

--- src/components/Carousel/CarouselTwo.jsx
+++ src/components/Carousel/CarouselTwo.jsx
@@ -18,35 +18,35 @@ export default function CarouselTwo() {
     setCurrentSlide((currentSlide + 1) % data.length);
   };
 
+  const handleDotClick = (index) => {
+    setCurrentSlide(index);
+  };
+
   return (
     <div className="mt-20 m-auto max-w-[700px] flex items-center flex-col">
       {data.map((item, index) => (
         <h1
           className={`${
             index === currentSlide ? "block" : "hidden"
-          } text-4xl font-bold text-yellow-600 mb-10`}
+          } text-4xl font-bold text-yellow-600 mb-5`}
           key={index}
         >
           {item}
         </h1>
       ))}
+
       <div className="flex items-center gap-3 bg-green-400 p-2">
-        <div
-          className={`${
-            currentSlide === 0 ? "border-slate-500" : "border-slate-50"
-          } w-10 h-1 border-2 `}
-        ></div>
-        <div
-          className={`${
-            currentSlide === 1 ? "border-slate-500" : "border-slate-50"
-          } w-10 h-1 border-2`}
-        ></div>
-        <div
-          className={`${
-            currentSlide === 2 ? "border-slate-500" : "border-slate-50"
-          } w-10 h-1 border-2`}
-        ></div>
+        {data.map((_, index) => (
+          <div
+            key={index}
+            className={`${
+              currentSlide === index ? "border-slate-500" : "border-slate-50"
+            } w-10 h-1 border-2 cursor-pointer`}
+            onClick={() => handleDotClick(index)}
+          ></div>
+        ))}
       </div>
+
       <div className="flex items-center justify-between w-full">
         <button
           className={`${
