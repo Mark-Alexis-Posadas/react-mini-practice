01af678068790fd3cdc086ce8e5f447daf744272
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-09 18:13:35 +0800

    added edit price objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-//Done - Render List of Products: Render a list of products from an array of product objects. Each product object should have properties like name, price, and description.
-
-import Objects from "./exercises/objects/Objects";
-
-//Done - Filter Products: Add buttons to filter the products based on their category. Categories could be stored as a property in the product objects.
-
-// Add to Cart: Create a button next to each product to add it to a shopping cart. Display the total number of items in the cart.
-
-// Remove from Cart: Add a button to each item in the shopping cart to remove it from the cart.
-
-// Toggle Product Availability: Render a list of products with a toggle button next to each product to toggle its availability (in stock/out of stock).
-
-// Sort Products: Add buttons to sort the list of products by price (ascending/descending) or name (alphabetically).
-
-// Render Comments: Render a list of comments from an array of comment objects. Each comment object should have properties like author, date, and content.
-
-// Add Comment Form: Create a form with input fields for author name and comment content. On submission, add the new comment to the list of comments.
-
-// Delete Comment: Add a button to each comment to delete it from the list of comments.
-
-// Like/Dislike Comment: Add buttons to each comment to like or dislike it. Display the number of likes and dislikes for each comment.
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Objects />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
---
+++ src/exercises/objects/ObjectsEditPrice.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { products } from "./exercises/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export default function ObjectsEditPrice() {
+  const [active, setActive] = useState(0);
+  const [product, setProduct] = useState(products);
+  const [inputValue, setInputValue] = useState("");
+  const [showInput, setShowInput] = useState(false);
+
+  const handleDelete = (id) => {
+    const onDelete = product.filter((item) => item.id !== id);
+    setProduct(onDelete);
+  };
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleEdit = (index) => {
+    const currentVal = product[index]; //curent price
+    setInputValue(currentVal.price);
+    setActive(index);
+    setShowInput(true);
+  };
+
+  const handleSubmit = () => {
+    if (inputValue.trim() === "") {
+      alert("Please add text");
+      return;
+    }
+    setProduct((currentVal) =>
+      currentVal.map((item, idx) =>
+        idx === active ? { ...item, price: inputValue } : item
+      )
+    );
+    setShowInput(false);
+  };
+  return (
+    <div className="p-10 w-[900px] m-auto relative">
+      <ul>
+        {product.map((item, index) => {
+          const { id, name, price, description, category, available } = item;
+
+          return (
+            <li
+              key={id}
+              className={`${
+                index === active && "border-green-500"
+              } border flex items-center gap-3 bg-slate-100 shadow-md  my-3 p-3 rounded`}
+            >
+              <h3 className="font-bold text-green-600">{name}</h3>
+              <p className="text-slate-500 text-sm">{description}</p>
+              <p>{category}</p>
+              <p className="font-bold text-md text-black">{price}</p>
+              <p className={available ? "text-green-600" : "text-red-600"}>
+                {available ? "In Stock" : "Out of stock"}
+              </p>
+              <div className="flex items-center gap-2 ml-auto">
+                <button
+                  className="rounded-full p-2 bg-blue-600 text-white w-10 h-10 flex items-center justify-center"
+                  onClick={() => handleEdit(index)}
+                >
+                  <FontAwesomeIcon icon={faPencilSquare} />
+                </button>
+                <button
+                  className="rounded-full p-2 bg-red-600 text-white w-10 h-10 flex items-center justify-center"
+                  onClick={() => handleDelete(id)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {showInput && (
+        <div className="fixed w-full h-full left-0 p-20 top-0 flex items-center justify-center bg-[rgba(0,0,0,0.4)]">
+          <div className="flex items-center gap-3">
+            <input
+              type="text"
+              className="bg-slate-100 p-3 rounded flex-1"
+              placeholder="Update Price"
+              value={inputValue}
+              onChange={handleInput}
+            />
+            <button
+              className="bg-red-600 text-white rounded p-3"
+              onClick={() => setShowInput(false)}
+            >
+              cancel
+            </button>
+            <button
+              className="bg-green-600 text-white rounded p-3"
+              onClick={handleSubmit}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
