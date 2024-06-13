a857a8bb0f1d75d5506995dd08af6786e70b1761
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-13 14:47:00 +0800

    done prev and next functionality

--- src/components/instagram-story-clone/Card.jsx
+++ src/components/instagram-story-clone/Card.jsx
@@ -1,13 +1,18 @@
 import { faGreaterThan, faLessThan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Card({ cardInfo }) {
+export default function Card({
+  cardInfo,
+  activeImage,
+  handleNext,
+  handlePrev,
+}) {
   return (
-    <div className="fixed left-0 w-full h-full flex items-center justify-center bg-[rgba(0,0,0,0.4)]">
+    <div className="fixed left-0 bottom-0 w-full h-full flex items-center justify-center bg-[rgba(0,0,0,0.4)]">
       <div className="bg-slate-200 shadow-md p-2 rounded w-[600px] h-[600px] relative">
         {cardInfo.images.map(
           (data, index) =>
-            index === 1 && (
+            index === activeImage && (
               <img
                 key={index}
                 src={data}
@@ -18,10 +23,20 @@ export default function Card({ cardInfo }) {
         )}
 
         <div className="flex items-center justify-between absolute top-[250px] w-full left-0">
-          <button className="w-8 h-8 rounded-full p-2 flex items-center justify-center bg-slate-100 text-gray-500">
+          <button
+            className={`${
+              activeImage === 0 ? "hidden" : "block"
+            } w-8 h-8 rounded-full p-2 flex items-center justify-center bg-slate-100 text-gray-500`}
+            onClick={handlePrev}
+          >
             <FontAwesomeIcon icon={faLessThan} />
           </button>
-          <button className="w-8 h-8 rounded-full p-2 flex items-center justify-center bg-slate-100 text-gray-500">
+          <button
+            className={`${
+              activeImage === 0 ? "ml-auto" : "ml-0"
+            } w-8 h-8 rounded-full p-2 flex items-center justify-center bg-slate-100 text-gray-500`}
+            onClick={handleNext}
+          >
             <FontAwesomeIcon icon={faGreaterThan} />
           </button>
         </div>
--- src/components/instagram-story-clone/InstagramStoryClone.jsx
+++ src/components/instagram-story-clone/InstagramStoryClone.jsx
@@ -3,14 +3,28 @@ import Card from "./Card";
 import Users from "./Users";
 import { usersInfo, storyCards } from "./data";
 export default function InstagramStoryClone() {
+  const [activeImage, setActiveImage] = useState(0);
   // const [showCard, setShowCard] = useState(null);
+  const handleNext = () => {
+    if (activeImage >= 2) {
+      return;
+    }
+    setActiveImage((prev) => prev + 1);
+  };
+
+  const handlePrev = () => {
+    if (activeImage <= 0) {
+      return;
+    }
+    setActiveImage((prev) => prev - 1);
+  };
 
   // const handleShowCard = (index) => {
   //   setShowCard(index);
   // };
 
   return (
-    <div className="mt-10 ml-10">
+    <div>
       <h1 className="text-8xl font-bold mb-5">Instagram stroy clone</h1>
       <ul className="flex items-center gap-3">
         {usersInfo.map((item, index) => (
@@ -23,8 +37,14 @@ export default function InstagramStoryClone() {
       </ul>
 
       <div className="flex items-center gap-3">
-        {storyCards.map((item) => (
-          <Card cardInfo={item} key={item.id} />
+        {storyCards.map((item, index) => (
+          <Card
+            cardInfo={item}
+            key={item.id}
+            activeImage={activeImage}
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+          />
         ))}
       </div>
     </div>
