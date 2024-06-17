c5e9c86c267825bdf4c6f044f08b864054553c37
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-17 11:21:25 +0800

    added forms in pet projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,7 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-const products = [
-  {
-    id: 1,
-    name: "Laptop",
-    price: 999,
-    description: "A high-performance laptop for work and gaming.",
-    category: "Electronics",
-    available: true,
-  },
-  {
-    id: 2,
-    name: "Headphones",
-    price: 149,
-    description: "Wireless headphones with noise-cancellation feature.",
-    category: "Electronics",
-    available: false,
-  },
-];
+import React from "react";
 
 const App = () => {
-  const [arr, setArr] = useState(products);
-
-  const handleAddArray = () => {
-    const newItem = {
-      id: uuidv4(),
-      name: "Alexis",
-      price: 1000,
-      description: "pogi",
-      category: "human",
-      available: true,
-    };
-
-    setArr((prev) => {
-      const updatedArr = [...prev, newItem];
-      console.log(updatedArr);
-      return updatedArr;
-    });
-
-    //OR
-
-    // setArr((prev) => [
-    //   ...prev,
-    //   {
-    //     id: uuidv4(),
-    //     name: "Alexis",
-    //     price: 1000,
-    //     description: "pogi",
-    //     category: "human",
-    //     available: true,
-    //     //OR newItem
-    //   },
-    // ]);
-  };
-
-  return (
-    <div>
-      {arr.map((item, index) => (
-        <div key={index}>
-          {item.name} - {item.id} - {item.price} - {item.description} -
-          {item.category}
-        </div>
-      ))}
-
-      <button
-        className="text-white bg-green-700 p-2 rounded"
-        onClick={handleAddArray}
-      >
-        Add array
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
--- src/pet/Pet.jsx
+++ src/pet/Pet.jsx
@@ -50,3 +50,125 @@ export const AppTwo = () => {
     </div>
   );
 };
+
+export const Loading = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (isLoading) {
+    return (
+      <>
+        <p>loading...</p>
+        <button
+          onClick={() =>
+            setTimeout(() => {
+              setIsLoading(false);
+              console.log(isLoading);
+            }, 3000)
+          }
+        >
+          click me
+        </button>
+      </>
+    );
+  }
+  return <div>App</div>;
+};
+
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [submittedValues, setSubmittedValues] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName.trim() || mi === "") {
+      alert("please fill out the form");
+      return;
+    }
+    setSubmittedValues([
+      ...submittedValues,
+      firstName,
+      lastName,
+      middleName,
+      userName,
+      password,
+      confirmPassword,
+    ]);
+    setFirstName("");
+    setLastName("");
+    setMiddleName("");
+    setUserName("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <div className="p-20">
+      {submittedValues.length === 0 ? null : (
+        <ul>
+          {sumittedValues.map((item, index) => (
+            <li
+              className="border border-b-0 last:border border-slate-500 p-2"
+              key={index}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+          type="text"
+          className="border border-black p-2 rounded my-2"
+          placeholder="first name"
+        />
+        <input
+          onChange={(e) => setMiddleName(e.target.value)}
+          value={middleName}
+          type="text"
+          className="border border-black p-2 rounded my-2"
+          placeholder="middle name"
+        />
+        <input
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+          type="text"
+          className="border border-black p-2 rounded my-2"
+          placeholder="last name"
+        />
+        <input
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+          type="text"
+          className="border border-black p-2 rounded my-2"
+          placeholder="user name"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          className="border border-black p-2 rounded my-2"
+          placeholder="password"
+        />
+        <input
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+          type="password"
+          className="border border-black p-2 rounded my-2"
+          placeholder="confirm password"
+        />
+        <button type="submit" className="text-white bg-black p-2 rounded mt-2">
+          submit
+        </button>
+      </form>
+    </div>
+  );
+}
