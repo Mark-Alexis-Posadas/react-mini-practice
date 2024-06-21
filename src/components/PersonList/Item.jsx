37bf8b2ecb48467333a196608bd730485fc1ceb3
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-21 19:41:21 +0800

    adjust item design

--- src/components/PersonList/Item.jsx
+++ src/components/PersonList/Item.jsx
@@ -22,27 +22,27 @@ export default function Item({
       key={index}
     >
       <ul>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           Name:
           <span className="text-black text-sm font-light">{name}</span>
         </li>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           Age:
           <span className="text-black text-sm font-light">{age}</span>
         </li>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           Gender:
           <span className="text-black text-sm font-light">{gender}</span>
         </li>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           Occupation:
           <span className="text-black text-sm font-light">{occupation}</span>
         </li>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           City:
           <span className="text-black text-sm font-light">{city}</span>
         </li>
-        <li className="font-bold text-purple-600 flex items-center gap-3">
+        <li className="font-bold text-purple-600 flex items-center gap-2">
           Country:
           <span className="text-black text-sm font-light">{country}</span>
         </li>
